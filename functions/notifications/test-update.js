ca1bfa950f24a0f0184078a5a33dd024f18d997c
azu <azuciao@gmail.com>
2016-07-26 01:06:56 +0900

    add update script

---
+++ functions/notifications/test-update.js
@@ -0,0 +1,21 @@
+// LICENSE : MIT
+"use strict";
+process.env.DEBUG = true;
+try {
+    var config = require('../../project.json').environment;
+    for (var key in config) {
+        process.env[key] = config[key]
+    }
+} catch (err) {
+    // ignore
+}
+process.env["force"] = true;
+const fn = require("./index").handle;
+fn({}, {
+    success: function () {
+        console.log("=Finish!");
+    },
+    fail: function (error) {
+        console.error(error);
+    }
+});
